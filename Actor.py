ed182f28f9a149779f52fc71d6f1bbfe29b319ce
Luke256 <yuta.k.200628@gmail.com>
2020-12-21 14:47:32 +0900

    Leanerのinit

--- Actor.py
+++ Actor.py
@@ -19,7 +19,7 @@ class Actor:
         myenv=False, #これをTrueにするとenv_nameにクラス名を渡せる
         epsilon=0.4,
         alpha=7,
-        window_length=1 #考慮に入れるフレーム数
+        window_length=4 #考慮に入れるフレーム数
         ):
 
         if(myenv):
@@ -27,6 +27,8 @@ class Actor:
         else:
             self.env=gym.make(env_name)
 
+        #各種変数の定義、代入
+        self.num_actions=self.env.action_space.n
         self.exp_queue=exp_queue
         self.param_queue=param_queue
         self.epochs=epochs
@@ -35,26 +37,33 @@ class Actor:
         self.epsilon=epsilon**(1+id_/num_actors*alpha)
         self.window_length=window_length
 
-        self.q_model=self.build_network() #価値計算用ネットワーク
+        self.main_Q=self.build_network() #行動決定用ネットワーク
+        self.target_Q=self.build_network() #価値計算用ネットワーク
         
+        #初期のLeanerの重みを取得
         while self.param_queue.empty():
             time.sleep(2)
-        
-        self.q_model.weights=self.param_queue.get()
+        self.main_Q.set_weights(self.param_queue.get()[0])
+        self.target_Q.set_weights(self.param_queue.get()[1])
 
     def build_network(self):
         #ネットワーク構築
+        # ここのネットワークはLeanerのものと一致させないといけない
+        # (コピペが良さそう)
         l_input=Input((self.window_length,)+self.env.observation_space.shape)
-        dense=Dense(units=256,activation="relu",name="dense_1_{}".format(self.id))(l_input)
-        dense=Dense(units=256,activation="relu",name="dense_2_{}".format(self.id))(dense)
-        dense=Dense(units=256,activation="relu",name="dense_3_{}".format(self.id))(dense)
-        v=Dense(units=256,activation="relu",name="dense_v_1_{}".format(self.id))(dense)
-        v=Dense(units=256,activation="relu",name="dense_v_2_{}".format(self.id))(v)
-        adv=Dense(units=256,activation="relu",name="dense_adv_1_{}".format(self.id))(dense)
-        adv=Dense(units=256,activation="relu",name="dense_adv_2_{}".format(self.id))(adv)
+        dense=Dense(units=256,activation="relu",name="dense1_{}".format(self.id))(l_input)
+        dense=Dense(units=256,activation="relu",name="dense2_{}".format(self.id))(dense)
+        dense=Dense(units=256,activation="relu",name="dense3_{}".format(self.id))(dense)
+        v=Dense(units=256,activation="relu",name="dense_v1_{}".format(self.id))(dense)
+        v=Dense(units=1,name="dense_v2_{}".format(self.id))(v)
+        adv=Dense(units=256,activation="relu",name="dense_adv1_{}".format(self.id))(dense)
+        adv=Dense(units=self.num_actions,name="dense_adv2_{}".format(self.id))(adv)
         y=concatenate([v,adv])
-        l_output=Lambda(lambda a: K.expand_dims(a[:, 0], -1) + a[:, 1:] - tf.compat.v1.stop_gradient(K.mean(a[:,1:],keepdims=True)), output_shape=(self.num_actions,))(y)
-        model=Model(input=l_input,output=l_output)
+        l_output=Lambda(lambda a: a - tf.stop_gradient(K.mean(a[:,1:],keepdims=True)), output_shape=(self.num_actions,))(y)
+        model=Model(inputs=l_input,outputs=l_output)
+        model.compile(optimizer="Adam",
+                        loss="mae",
+                        metrics=["accuracy"])
 
         return model
 
--- Leaner.py
+++ Leaner.py
@@ -1,1 +1,54 @@
-class 
+import keras.backend as K
+import tensorflow as tf
+from keras import Model
+from keras.layers import Dense, Input, Lambda, concatenate
+from game import GameClass
+import gym
+
+
+class Leaner:
+    def __init__(self,
+        exp_queue, #Memory
+        param_queue, #Actorへの提供用
+        exp_memory_size, #Memoryの上限(MemoryもLeanerで管理する)
+        train_mamory_size, #学習するときのバッチサイズ
+        env, #トレーニング環境
+        myenv=False, #クラスか名前か
+        window_length=4 #考慮に入れるフレーム数
+        ):
+        self.exp_queue=exp_queue
+        self.param_queue=param_queue
+        self.exp_memory_size=exp_memory_size
+        self.train_mamory_size=train_mamory_size
+        self.window_length=window_length
+        if myenv:
+            self.env=env()
+        else:
+            self.env=gym.make(env)
+        self.num_actions=self.env.action_space.n
+
+        self.main_Q=self.build_network() #行動決定用のQネットワーク
+        self.target_Q=self.build_network() #価値計算用のQネットワーク
+
+    def build_network(self):
+        #ネットワーク構築
+        l_input=Input((self.window_length,)+self.env.observation_space.shape)
+        dense=Dense(units=256,activation="relu",name="dense1_leaner")(l_input)
+        dense=Dense(units=256,activation="relu",name="dense2_leaner")(dense)
+        dense=Dense(units=256,activation="relu",name="dense3_leaner")(dense)
+        v=Dense(units=256,activation="relu",name="dense_v1_leaner")(dense)
+        v=Dense(units=1,name="dense_v2_leaner")(v)
+        adv=Dense(units=256,activation="relu",name="dense_adv1_leaner")(dense)
+        adv=Dense(units=self.num_actions,name="dense_adv2_leaner")(adv)
+        y=concatenate([v,adv])
+        l_output=Lambda(lambda a: a - tf.stop_gradient(K.mean(a[:,1:],keepdims=True)), output_shape=(self.num_actions,))(y)
+        model=Model(inputs=l_input,outputs=l_output)
+        model.compile(optimizer="Adam",
+                        loss="mae",
+                        metrics=["accuracy"])
+
+        return model
+
+
+if __name__=="__main__":
+    l=Leaner(1,1,1,1,GameClass,myenv=True)
